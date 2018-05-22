b9b48aecc087c578283a26fea0f79004c425e3d1
540725 <540725@PHS-CTE01-21.cuhsd-int.prv>
2018-05-22 09:07:17 -0700

    a

--- SharedFinal/src/park/Attraction.java
+++ SharedFinal/src/park/Attraction.java
@@ -1,15 +1,23 @@
 package park;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Collections;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Attraction {
+	private final Properties properties;
+	
 	public enum AttributeTypes{
 		NUMBER_OF_RIDERS,
 		TICKET_PRICE;
 	}
-	private final Map<AttributeTypes, Object> attributes;
+	
+	//private final Map<AttributeTypes, Object> attributes;
 	 //public final int maxRiders;
 	 //public final double rideDuration;
 	 //public final int maxConcurrentRides;
@@ -20,15 +28,37 @@ public class Attraction {
 	 //public final double thrillRating;
 	 public final String name;
 	 private double ridesToday = 0;
-	 public Attraction(String name, Map<AttributeTypes, Object> attributes){
+	 public Attraction(String name, File attributes) throws IOException {
 		this.name=name;
-		this.attributes = Collections.unmodifiableMap(attributes);
+		properties = new Properties();
+		try(FileInputStream fin = new FileInputStream(attributes)) {
+			properties.load(fin);
+		}
 	 }
 	 public String getName() {
 		 return this.name;
 	 }
 	 
 	 public Map<AttributeTypes, Object> getAttributes() {
-		 return attributes;
+		 HashMap<AttributeTypes, Object> m = new HashMap<AttributeTypes, Object>();
+		 properties.forEach((Object key, Object value) -> {
+			 if(key instanceof String)
+				 m.put(AttributeTypes.valueOf((String)key), value);
+			 else if(key instanceof AttributeTypes)
+				 m.put((AttributeTypes) key, value);
+			 else throw new Error("Property keys must be either strings or instances of AttributeTypes");
+		 });
+		 return m;
 	 }
+	void setAttribute(AttributeTypes attribute, Object newValue) {
+		 properties.put(attribute, newValue);
+	 }
+	
+	void saveToFile(File f) throws IOException {
+		try(FileOutputStream fout = new FileOutputStream(f)) {
+			properties.store(fout, name);
+		}
+	}
+	 
+	 
 }
