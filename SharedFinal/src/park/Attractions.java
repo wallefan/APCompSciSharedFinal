f7f6defc7cbca3ad29789fc6cbd4b2f50d750b9d
adrianmgg <2adrian2@gmail.com>
2018-05-24 08:35:26 -0700

    added

---
+++ SharedFinal/src/park/Attractions.java
@@ -0,0 +1,14 @@
+package park;
+
+import java.util.ArrayList;
+import park.Attraction.AttributeTypes;
+
+public class Attractions {
+	static ArrayList<Attraction> attractions = new ArrayList<Attraction>();
+	static Attraction wheel = new Attraction("Wheely");
+	static {
+		attractions.add(wheel);
+		wheel.setAttribute(AttributeTypes.NUMBER_OF_RIDERS,100);
+		wheel.setAttribute(AttributeTypes.TICKET_PRICE,50.00);
+	}
+}
