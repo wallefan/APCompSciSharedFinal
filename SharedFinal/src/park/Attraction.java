2cf227edde9ed653d199816141f0c4ea1aa5d5ca
540725 <540725@PHS-CTE01-21.cuhsd-int.prv>
2018-05-24 08:29:29 -0700

    Fixed file saving and loading

--- SharedFinal/src/park/Attraction.java
+++ SharedFinal/src/park/Attraction.java
@@ -13,8 +13,9 @@ import java.util.Properties;
 import javax.swing.text.html.parser.Entity;
 
 public class Attraction {
-	//private final Properties properties;
-	private final HashMap<AttributeTypes,Object> attributes = new HashMap<AttributeTypes,Object>();;
+	// private final Properties properties;
+	private final HashMap<AttributeTypes, Object> attributes = new HashMap<AttributeTypes, Object>();;
+
 	public enum AttributeTypes {
 		NUMBER_OF_RIDERS(0), TICKET_PRICE(0.0);
 		Object defaultVal;
@@ -38,8 +39,8 @@ public class Attraction {
 
 	public Attraction(String name) {
 		this.name = name;
-		for(AttributeTypes attr:AttributeTypes.values()) {
-			attributes.put(attr,attr.defaultVal);
+		for (AttributeTypes attr : AttributeTypes.values()) {
+			attributes.put(attr, attr.defaultVal);
 		}
 	}
 
@@ -49,40 +50,73 @@ public class Attraction {
 		try (FileInputStream fin = new FileInputStream(file)) {
 			properties.load(fin);
 		}
-		for(Entry<Object,Object> entry:properties.entrySet()) {
-			attributes.put((AttributeTypes)entry.getKey(),entry.getValue());
+		for (Entry<Object, Object> entry : properties.entrySet()) {
+			attributes.put((AttributeTypes) entry.getKey(), entry.getValue());
 		}
 	}
 
 	public String getName() {
 		return this.name;
 	}
-	
-	public HashMap<AttributeTypes,Object> getAttributes(){
+
+	public HashMap<AttributeTypes, Object> getAttributes() {
 		return attributes;
 	}
 
-//	public Map<AttributeTypes, Object> getAttributes() {
-//		HashMap<AttributeTypes, Object> m = new HashMap<AttributeTypes, Object>();
-//		attributes.forEach((AttributeTypes key, Object value) -> {
-//			if (key instanceof String)
-//				m.put(AttributeTypes.valueOf((String) key), value);
-//			else if (key instanceof AttributeTypes)
-//				m.put((AttributeTypes) key, value);
-//			else
-//				throw new Error("Property keys must be either strings or instances of AttributeTypes");
-//		});
-//		return m;
-//	}
+	// public Map<AttributeTypes, Object> getAttributes() {
+	// HashMap<AttributeTypes, Object> m = new HashMap<AttributeTypes, Object>();
+	// attributes.forEach((AttributeTypes key, Object value) -> {
+	// if (key instanceof String)
+	// m.put(AttributeTypes.valueOf((String) key), value);
+	// else if (key instanceof AttributeTypes)
+	// m.put((AttributeTypes) key, value);
+	// else
+	// throw new Error("Property keys must be either strings or instances of
+	// AttributeTypes");
+	// });
+	// return m;
+	// }
 
 	void setAttribute(AttributeTypes attribute, Object newValue) {
 		attributes.put(attribute, newValue);
 	}
 
 	void saveToFile(File f) throws IOException {
+		Properties p = new Properties();
+		// you ever just been reading the docs and decided you really, really hate Java?
+
+		// "Because Properties inherits from Hashtable, the put and putAll methods can
+		// be
+		// applied to a Properties object. Their use is strongly discouraged as they
+		// allow the caller to insert entries whose keys or values are not Strings. The
+		// setProperty method should be used instead. If the store or save method is
+		// called on a "compromised" Properties object that contains a non-String key or
+		// value, the call will fail." WHY ISN'T IT A HashMap<String, String> THEN!?
+		// It would be so easy to avoid this whole problem! Programming languages don't
+		// NEED bureaucracy!
+		
+		// Oh well...
+
+		attributes.forEach((AttributeTypes k, Object v) -> p.setProperty(k.toString(), v.toString()));
 		try (FileOutputStream fout = new FileOutputStream(f)) {
-//			properties.store(fout, name);
+			p.store(fout, name);
 		}
+	}
+
+	void loadFromFile(File f) throws IOException {
+		Properties p = new Properties();
+		try (FileInputStream fin = new FileInputStream(f)) {
+			p.load(fin);
+		}
+		p.forEach((Object k, Object v) -> {
+			if (k instanceof AttributeTypes)
+				attributes.put((AttributeTypes) k, v);
+			else if (k instanceof String)
+				attributes.put(AttributeTypes.valueOf((String) k), v);
+			else
+				throw new Error("Key types must be either AttributeTypes or Strings representing them");
+		});
+
 	}
 
 }
